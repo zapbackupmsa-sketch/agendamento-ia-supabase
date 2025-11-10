0eb378a241668d6f331d747eeb099d5869204b9b
zapbackupmsa-sketch <zapbackupmsa@gmail.com>
2025-11-10 09:02:25 -0300

    Create Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  generateEtags: true,
+};
+
+module.exports = nextConfig;
